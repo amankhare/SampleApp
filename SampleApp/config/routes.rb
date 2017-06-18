9dc9fd3251107c29fabbb785e72df42607a0ea6d
amankhare <amankhr141993@gmail.com>
2017-06-18 13:19:19 +0530

    adding modified files for static pages

--- SampleApp/config/routes.rb
+++ SampleApp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'static_pageshome/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
